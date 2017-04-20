e38eeb48d65eb0d7db02eb437c7dc69584d113d1
WMarchewka <waltmarchewka@gmail.com>
2017-04-20 14:37:42 -0400

    -

---
+++ upload.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+require '.\unit_upload.pl';
+
+$unit_ip = shift;
+$user = shift;
+$password = shift;
+$slot = shift;
+$file = shift;
+upload_unit($unit_ip,$user,$password,$slot,$file);
+
