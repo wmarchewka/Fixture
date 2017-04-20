8a09245ae3524c5d4feeb88d7d4e23157313a438
Walter Marchewka <waltmarchewka@gmail.com>
2017-04-20 08:13:54 -0400

    changed telnet to work

--- TelnetM40ButtonTest.py
+++ TelnetM40ButtonTest.py
@@ -3,25 +3,27 @@ import time
 
 def TelnetM40ButtonTest():
     try:
-        HOST = "192.168.1.6"
-        #HOST = '192.168.1.99'
+        #HOST = "192.168.1.6"
+        HOST = '10.0.0.210'
         print('Starting button test on ' + HOST)
         PORT = 23
         TIMEOUT = 5
         tn = telnetlib.Telnet(host=HOST, port=PORT, timeout=TIMEOUT)
         tn.write(b'$login,factory,factory\n')
         tn.write(b"$bts,s,0\n")
-        tn.write(b"$bts,G\n\r")
-        ret = tn.read_all()
-        print(ret)
+        #tn.write(b"$bts,G\n\r")
+        #ret = tn.read_all()
+        #print(ret)
+        t = 0
         while True:
             tn.write(b"$bts,G\n")
             time.sleep(1)
-            tn.write(b"$\r")
-            ret = tn.read_all()
-            print(ret)
-            time.sleep(1)
-        tn.close()
+            t = t + 1
+            if t == 3:
+                tn.write(b"\r")
+                ret = tn.read_all()
+                print(ret)
+                #tn.close()
     except OSError as err:
         #tn.close
         print(err)
