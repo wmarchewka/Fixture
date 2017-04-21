c710e806f7a3873adb88a032ba85728298a39e7e
wmarchewka <waltmarchewka@gmail.com>
2017-04-21 12:34:58 -0400

    -

--- TelnetUploadWifiFirmware.py
+++ TelnetUploadWifiFirmware.py
@@ -2,6 +2,7 @@ import socket
 import os
 import base64
 import GetOS as gs
+import sys
 
 global osname
 osname = gs.get_platform()
@@ -20,38 +21,45 @@ def upload_file(host,fname,slot,path):
         sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sc.settimeout(5)
         conn = host, port
-        # sc.connect(conn)
+        sc.connect(conn)
 
         fsize = file_size(path)
-        filename = open(path, "rb")
+        fsize = 123632
+        filename = open(path, "br")
         fn = filename.read(fsize)
-
 
         myauthorization = base64.b64encode(b'factory:factory')
 
         my_req_body = "-----------------------------7dd3201c5104d4\r\n"
-        my_req_body = my_req_body  + "Content-Disposition: form-data; name=\\" + str(slot) + "\\; filename=\\" + str(fname) + "\r\n"
-        my_req_body = my_req_body  + "Content-Type: application/octet-stream\r\n"
-        my_req_body = my_req_body  + "\r\n"
+        my_req_body = my_req_body  + 'Content-Disposition: form-data; name="' + str(slot) + '"; filename="' + str(path) + '"\r\n'
+        my_req_body = my_req_body  + 'Content-Type: application/octet-stream\r\n'
+        my_req_body = my_req_body  + '\r\n'
 
         my_req_body = my_req_body + str(fn)
 
-        my_req_body = my_req_body+  "-----------------------------7dd3201c5104d4\r\n"
+        my_req_body = my_req_body +  "-----------------------------7dd3201c5104d4\r\n"
         fsize = file_size(path)
-
+        fsize = '123632'
         my_req_head = "POST /upload_file.cgi HTTP/1.1\r\n"
-        my_req_head = my_req_head + "Accept-Language: en-us\r\n"
-        my_req_head = my_req_head + "Host: 192.168.1.100\r\n"
+        my_req_head = my_req_head +  "Accept-Language: en-us\r\n"
+        #print(my_req_head)
+        my_req_head = my_req_head + "Host: 192.168.1.8\r\n"
         my_req_head = my_req_head + "Content-Type: multipart/form-data; boundary=---------------------------7dd3201c5104d4\r\n"
         my_req_head = my_req_head + "Content-Length: " + str(fsize) + "\r\n"
         my_req_head = my_req_head + "Connection: Keep-Alive\r\n"
-        my_req_head = my_req_head + "Authorization: Basic " + str(myauthorization)
+        my_req_head = my_req_head + "Authorization: Basic ZmFjdG9yeTpmYWN0b3J5"# + str(myauthorization)
         my_req_head = my_req_head + "\r\n\r\n"
         my_req_head = my_req_head + "\r\n"
 
-        print(my_req_head + my_req_body)
+        sys.stdout.write(my_req_head)
+        sys.stdout.write(my_req_body)
 
-        #sc.send(my_req_head + my_req_body)
+        total =  my_req_head + my_req_body
+        sc.send(total.encode('ascii'))
+
+        data = sc.recv(500)
+        print('return data-------------------------')
+        print(data)
 
 
     except OSError as err:
@@ -63,8 +71,8 @@ def main():
 
     global osname
     host = '192.168.1.8'
-    host = '10.0.0.210'
-    slot = 'wifi'
+    #host = '10.0.0.210'
+    slot = 'web'
     print(osname)
 
     if osname == "Windows":
