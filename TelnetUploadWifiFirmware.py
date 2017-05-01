aeef1f432a5e6d480136cd392cd4dd5e7aff92cc
wmarchewka <waltmarchewka@gmail.com>
2017-05-01 15:01:00 -0400

    -

--- TelnetUploadWifiFirmware.py
+++ TelnetUploadWifiFirmware.py
@@ -24,22 +24,33 @@ def upload_file(host,fname,slot,path):
         sc.connect(conn)
 
         fsize = file_size(path)
-        fsize = 123632
+        print('fsize->' + str(fsize))
         filename = open(path, "br")
-        fn = filename.read(fsize)
+        fn = filename.read()
 
         myauthorization = base64.b64encode(b'factory:factory')
 
         my_req_body = "-----------------------------7dd3201c5104d4\r\n"
-        my_req_body = my_req_body  + 'Content-Disposition: form-data; name="' + str(slot) + '"; filename="' + str(fname) + '"\r\n'
+        my_req_body = my_req_body  + 'Content-Disposition: form-data; name="' + str(slot) + '"; filename="' + str(path) + '"\r\n'
         my_req_body = my_req_body  + 'Content-Type: application/octet-stream\r\n'
-        my_req_body = my_req_body  + '\r\n'
+        my_req_body = my_req_body  + '\r\n\r\n'
+        body_len = len(my_req_body)
+        print('body length ' + str(body_len))
 
-        my_req_body = my_req_body + str(fn)
+        print('string file length ' + str(len(str(fn))))
 
-        my_req_body = my_req_body +  "-----------------------------7dd3201c5104d4\r\n"
-        fsize = file_size(path)
-        fsize = '123632'
+        my_req_body = my_req_body + fn
+
+        file_len = len(fn)
+        print('file length ' + str(file_len))
+        my_req_body = my_req_body +  "\r\n-----------------------------7dd3201c5104d4\r\n"
+        body_len = len(my_req_body)
+        print('body length ' + str(body_len))
+        end_len = len('\r\n-----------------------------7dd3201c5104d4\r\n')
+        print('end length ' + str(end_len))
+
+        #fsize = file_size(path)
+        #fsize = '123632'
         my_req_head = "POST /upload_file.cgi HTTP/1.1\r\n"
         my_req_head = my_req_head +  "Accept-Language: en-us\r\n"
         #print(my_req_head)
@@ -55,7 +66,7 @@ def upload_file(host,fname,slot,path):
         sys.stdout.write(my_req_body)
 
         total =  my_req_head + my_req_body
-        sc.send(total.encode('ascii'))
+        #sc.send(total.encode('ascii'))
 
         data = sc.recv(500)
         print('return data-------------------------')
@@ -70,13 +81,13 @@ def upload_file(host,fname,slot,path):
 def main():
 
     global osname
-    #host = '192.168.1.5'
-    host = '10.0.0.148'
+    host = '192.168.1.99'
+    #host = '10.0.0.148'
     slot = 'web'
     print(osname)
 
     if osname == "Windows":
-        path = r"C:\UEC\Functional Test\M50\Configuration\web_pages_UEC025_ENG.tfs"
+        path = r"C:web_pages_UEC025_ENG.tfs"
     elif osname == "OS X":
         path = os.path.expanduser("~/web_pages_UEC025_ENG.tfs")
 
