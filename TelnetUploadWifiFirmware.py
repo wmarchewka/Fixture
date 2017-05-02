0921d7b097484254accf6c02fed29c5ea1bc4c30
Walter Marchewka <waltmarchewka@gmail.com>
2017-05-02 06:19:23 -0400

    e

--- TelnetUploadWifiFirmware.py
+++ TelnetUploadWifiFirmware.py
@@ -11,7 +11,7 @@ osname = gs.get_platform()
 def file_size(fname):
     import os
     statinfo = os.stat(fname)
-    print('filesize->' + str(statinfo.st_size))
+    #print('filesize->' + str(statinfo.st_size))
     return statinfo.st_size
 
 def upload_file(host,fname,slot,path):
@@ -24,39 +24,30 @@ def upload_file(host,fname,slot,path):
         sc.connect(conn)
 
         fsize = file_size(path)
-        print('fsize->' + str(fsize))
+        #print('fsize->' + str(fsize))
         filename = open(path, "br")
         fn = filename.read()
 
         myauthorization = base64.b64encode(b'factory:factory')
 
         my_req_body = "-----------------------------7dd3201c5104d4\r\n"
-        my_req_body = my_req_body  + 'Content-Disposition: form-data; name="' + str(slot) + '"; filename="' + str(path) + '"\r\n'
-        my_req_body = my_req_body  + 'Content-Type: application/octet-stream\r\n'
-        my_req_body = my_req_body  + '\r\n\r\n'
-        body_len = len(my_req_body)
-        print('body length ' + str(body_len))
+        my_req_body =  my_req_body + 'Content-Disposition: form-data; name="' + str(slot) + '"; filename="' + str(path) + '"\r\n'
+        my_req_body =  my_req_body + 'Content-Type: application/octet-stream\r\n'
+        my_req_body =  my_req_body + '\r\n\r\n'
+        my_req_end = "\r\n-----------------------------7dd3201c5104d4\r\n"
 
-        print('string file length ' + str(len(str(fn))))
+        print('Length of body->' + str(len(my_req_body)))
+        print('Length of file->' + str(fsize))
+        print('Length of end->' + str(len(my_req_end)))
 
-        my_req_body = my_req_body + fn
+        totalsize = len(my_req_body) + fsize + len(my_req_end)
+        print('Total size->' + str(totalsize))
 
-        file_len = len(fn)
-        print('file length ' + str(file_len))
-        my_req_body = my_req_body +  "\r\n-----------------------------7dd3201c5104d4\r\n"
-        body_len = len(my_req_body)
-        print('body length ' + str(body_len))
-        end_len = len('\r\n-----------------------------7dd3201c5104d4\r\n')
-        print('end length ' + str(end_len))
-
-        #fsize = file_size(path)
-        #fsize = '123632'
         my_req_head = "POST /upload_file.cgi HTTP/1.1\r\n"
         my_req_head = my_req_head +  "Accept-Language: en-us\r\n"
-        #print(my_req_head)
         my_req_head = my_req_head + "Host: 192.168.1.100\r\n"
         my_req_head = my_req_head + "Content-Type: multipart/form-data; boundary=---------------------------7dd3201c5104d4\r\n"
-        my_req_head = my_req_head + "Content-Length: " + str(fsize) + "\r\n"
+        my_req_head = my_req_head + "Content-Length: " + str(totalsize) + "\r\n"
         my_req_head = my_req_head + "Connection: Keep-Alive\r\n"
         my_req_head = my_req_head + "Authorization: Basic ZmFjdG9yeTpmYWN0b3J5"# + str(myauthorization)
         my_req_head = my_req_head + "\r\n\r\n"
@@ -64,9 +55,14 @@ def upload_file(host,fname,slot,path):
 
         sys.stdout.write(my_req_head)
         sys.stdout.write(my_req_body)
+        sys.stdout.write(str(fn))
+        sys.stdout.write(str(my_req_end))
 
-        total =  my_req_head + my_req_body
-        #sc.send(total.encode('ascii'))
+        sc.send(my_req_head.encode('ascii'))
+        sc.send(my_req_body.encode('ascii'))
+        sc.send(fn)
+        sc.send(my_req_end.encode('ascii'))
+
 
         data = sc.recv(500)
         print('return data-------------------------')
@@ -81,8 +77,8 @@ def upload_file(host,fname,slot,path):
 def main():
 
     global osname
-    host = '192.168.1.99'
-    #host = '10.0.0.148'
+    #host = '192.168.1.99'
+    host = '10.0.0.229'
     slot = 'web'
     print(osname)
 
