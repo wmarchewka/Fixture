82fd5e2d43015018af873973c0b3c43f93d87d7e
wmarchewka <waltmarchewka@gmail.com>
2017-06-22 15:00:14 -0400

    -

--- ProgrammersLibrary.py
+++ ProgrammersLibrary.py
@@ -20,8 +20,8 @@ def CycloneProgram(self, port, image):
             return False, 'Please select Cyclone serial port'
 
         try:
-            logger.debug('Looking for Cyclone programmer...')
-            self.lblStatus.setText('Looking for Cyclone programmer...')
+            logger.debug('Looking for Cyclone programmer on port ' + str(port))
+            self.lblStatus.setText('Looking for Cyclone programmer on port '+ str(port))
             time.sleep(1)
             ser = serial.Serial()#(port, 115200, timeout=1)
             ser.setRTS(False)           #needed to ensure the DTR or RTS line doesnt reset programmer on startup
@@ -39,10 +39,16 @@ def CycloneProgram(self, port, image):
         ser.write(b'\x03\x01\x18\x5d')          # first command
         logger.debug('SEND->\\x03\\x01\\x18\\x5d')     #
         line = ser.read(2)                      # response should be 01 00
+        if line == b'':
+            self.lblStatus.setText('Cyclone programmer not responding')
+            return False, 'Cyclone programmer not responding'
         logger.debug('RECV->' + str(line))
-        ser.write(b'\x03\x01\x0B\x24')  # write command
+        ser.write(b'\x03\x01\x0B\x24')          # write command
         logger.debug('SEND->\\x03\\x01\\x0B\\x24')
         line = ser.readline()
+        if line == b'':
+            self.lblStatus.setText('Cyclone programmer not responding')
+            return False, 'Cyclone programmer not responding'
         logger.debug('RECV->' + str(line))
         if line == cyclone_recv1 or cyclone_recv2:
             logger.debug('RECV->' + str(line))
@@ -55,12 +61,18 @@ def CycloneProgram(self, port, image):
             if image==4:
                 ser.write(b'\x04\x18\x1c\x04\x44')  # command to select iamge 4
             line = ser.readline()
+            if line == b'':
+                self.lblStatus.setText('Cyclone programmer not responding')
+                return False, 'Cyclone programmer not responding'
             logger.debug('RECV->' + str(line))
             logger.debug('Cyclone programming Image ' +  str(image))
             self.lblStatus.setText('Cyclone programming Image ' +  str(image))
             ser.write(b'\x03\x18\x41\x3f')  # command to start programming
             logger.debug('SEND->\\x03\\x18\\x41\\x3f')
             line = ser.read(2)  # Check for new line and CR     # response should be 01 ee
+            if line == b'':
+                self.lblStatus.setText('Cyclone programmer not responding')
+                return False, 'Cyclone programmer not responding'
             logger.debug('RECV->' + str(line))
             finished = False
             # repeat this asking for status until cyclone responds
@@ -69,18 +81,23 @@ def CycloneProgram(self, port, image):
                 time.sleep(0.5)
                 logger.debug('SEND->\\x03\\x18\\x5f\\x65')
                 line = ser.readline()
+                if line == b'':
+                    self.lblStatus.setText('Cyclone programmer not responding')
+                    return False, 'Cyclone programmer not responding'
                 logger.debug('RECV->' + str(line))
                 if line == b'\x03\x01\x01\xee':          #responce will be 03 00 00 ee until we recv a 03 01 01 ee
                     ser.write(b'\x03\x18\x33\x66')       # send to get status
                     logger.debug('SEND->\\x03\\x18\\x33\\x66')
                     line = ser.readline()
+                    if line == b'':
+                        self.lblStatus.setText('Cyclone programmer not responding')
+                        return False, 'Cyclone programmer not responding'
                     logger.debug('RECV->' + str(line))
                     if line == b'\x03\x00\x00\xee':
                         logger.debug('Cyclone Success')
                         ser.close()
                         return True, 'Cyclone Programmer Success'
                         self.lblStatus.setText('Cyclone Programmer Success')
-
                     else:
                         logger.debug("Error " + str(line[2]))
                         ser.close()
